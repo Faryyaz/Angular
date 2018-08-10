44424cf9366366fcc50221c60c12eefdc6b962f2
Faryyaz <dilfary@hotmail.com>
2018-08-11 00:50:28 +0400

    Component edits

--- first-app/src/app/servers/servers.component.ts
+++ first-app/src/app/servers/servers.component.ts
@@ -2,7 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // selector: '[app-servers]', // select by attribute
+  // selector: '.app-servers', // select by class
   templateUrl: './servers.component.html',
+  // template: `
+  //   <app-servers></app-servers> // inline template can be used as well
+  // `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
