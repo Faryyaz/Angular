2af0fb7e58714450c2390c3c316a50cebe479faa
DILMAHOMED Faryyaz OBS/OCB <hrhk5567@orange.com>
2018-08-13 17:46:30 +0200

    Component updated

--- practice-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ practice-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'Only a test',
+      'https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe-600x629.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'Only a test',
+      'https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe-600x629.jpg'
+    )
+  ];
 
   constructor() { }
 
