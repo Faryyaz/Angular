c77c144dfb425486a0b1fc1ad910ddacb7b81a0f
DILMAHOMED Faryyaz OBS/OCB <hrhk5567@orange.com>
2018-08-27 12:57:34 +0200

    Route separated

---
+++ routing-app/src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from "../../node_modules/@angular/core";
+import { HomeComponent } from "./home/home.component";
+import { ServersComponent } from "./servers/servers.component";
+import { ServerComponent } from "./servers/server/server.component";
+import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { UsersComponent } from "./users/users.component";
+import { UserComponent } from "./users/user/user.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { Routes, RouterModule } from "../../node_modules/@angular/router";
+
+const appRoutes: Routes =[
+    {path: '', component: HomeComponent},
+    {path: 'servers', component: ServersComponent, children: [
+      {path: ':id', component: ServerComponent},
+      {path: ':id/edit', component: EditServerComponent}
+    ]},
+    {path: 'users', component: UsersComponent, children: [
+      {path: ':id/:name', component: UserComponent}, // pass parameter through the url using colon :
+    ]},
+    {path: 'not-found', component: PageNotFoundComponent},
+    {path: '**', redirectTo: '/not-found'} // should always be last in the array. if url is not found then 404 and redirect to path
+  ];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule {
+
+}
--- routing-app/src/app/app.module.ts
+++ routing-app/src/app/app.module.ts
@@ -11,21 +11,10 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
-const appRoutes: Routes =[
-  {path: '', component: HomeComponent},
-  {path: 'servers', component: ServersComponent, children: [
-    {path: ':id', component: ServerComponent},
-    {path: ':id/edit', component: EditServerComponent}
-  ]},
-  {path: 'users', component: UsersComponent, children: [
-    {path: ':id/:name', component: UserComponent}, // pass parameter through the url using colon :
-  ]},
-  {path: 'not-found', component: PageNotFoundComponent},
-  {path: '**', redirectTo: '/not-found'} // should always be last in the array. if url is not found then 404 and redirect to path
-];
+
 
 @NgModule({
   declarations: [
@@ -42,7 +31,7 @@ const appRoutes: Routes =[
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
