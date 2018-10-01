9cdb65497f41c65f74e4af85965fdc9d561eb88c
Faryyaz <dilfary@hotmail.com>
2018-10-01 16:22:44 +0400

    Observable package example with different state

--- observables-app/src/app/home/home.component.ts
+++ observables-app/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { interval } from 'rxjs';
-// import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
 
 
 @Component({
@@ -13,12 +14,34 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const myNumbers = interval(1000);
-    myNumbers.subscribe(
-      (number: number) => {
-        console.log(number);
+    // const myNumbers = interval(1000);
+    // myNumbers.subscribe(
+    //   (number: number) => {
+    //     console.log(number);
+    //   }
+    // );
+
+    const myObservable = Observable.create(
+      (observer: Observer<string>) => {
+        setTimeout( () => { observer.next('First package') } , 2000);
+        setTimeout( () => { observer.next('second package') } , 4000);
+        setTimeout( () => { 
+          // observer.error('This does not work') 
+          observer.complete();
+        } , 5000);
+        setTimeout( () => { 
+          // this should not work 
+          observer.next('third package');
+        } , 6000);
       }
     );
+
+    myObservable.subscribe(
+      (data: string) => { console.log(data); },
+      (error: string) => { console.log(error);  },
+      () => { console.log( 'completed' ); }
+    );
+
   }
 
 }
