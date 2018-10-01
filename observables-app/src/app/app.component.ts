20f431a391200dca744c1e2d95e736af724bd6be
Faryyaz <dilfary@hotmail.com>
2018-10-01 16:46:18 +0400

    Subscription as a service/ works like observable

--- observables-app/src/app/app.component.ts
+++ observables-app/src/app/app.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  user1Activated = false;
+  user2Activated = false;
+
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit() {
+    this.usersService.userActivated.subscribe(
+      (id: number) => {
+        if ( id === 1) {
+          this.user1Activated = true;
+        } else {
+          this.user2Activated = true;
+        }
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    // good practice to implement unsubcribe if ever we navigate away from this component within our app
+    this.usersService.userActivated.unsubscribe();
+  }
+
+
 }
---
+++ observables-app/src/app/users.service.ts
@@ -0,0 +1,6 @@
+import { Subject } from "../../node_modules/rxjs/Subject";
+
+
+export class UsersService {
+    userActivated = new Subject();
+}
