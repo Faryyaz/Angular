e78377aa3426e05358d6dab49cc8230a5f2b65e6
Faryyaz <dilfary@hotmail.com>
2018-10-02 10:40:36 +0400

    Added radio button

--- forms-app/src/app/app.component.ts
+++ forms-app/src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   @ViewChild('f') form: NgForm;
   defaultQuestion = 'pet';
   answer = '';
+  genders = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
