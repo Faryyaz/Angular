d73ff875b5da239d830457a0ed3533e52e004e09
Faryyaz <dilfary@hotmail.com>
2018-10-02 19:31:30 +0400

    Submitted method implemented

--- forms-app/src/app/app.component.ts
+++ forms-app/src/app/app.component.ts
@@ -7,13 +7,40 @@ import { NgForm } from '../../node_modules/@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f') form: NgForm;
+  @ViewChild('f') userForm: NgForm;
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secret: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
+
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+
+    // not a good way as other values will be overwritten as well
+    // this.userForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   answer: '',
+    //   gender: 'male'
+    // });
+
+    this.userForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+
   }
 
   // this should be used when (ngSubmit)="onSubmit(f)" on the html form
@@ -22,7 +49,12 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.form);
+    this.submitted = true;
+    this.user.username = this.userForm.value.userData.username;
+    this.user.email = this.userForm.value.userData.email;
+    this.user.secret = this.userForm.value.secret;
+    this.user.answer = this.userForm.value.answer;
+    this.user.gender = this.userForm.value.gender;
   }
 
 }
